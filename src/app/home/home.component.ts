7d00b8dbbbbf409bf0ba53c88bb1c7c52f145dd0
Lộc <locpham050100@gmail.com>
2023-05-13 23:47:42 +0700

    *ngFor (phần tử là Object). Kết hợp chút HTML/Css, chút *ngIf, [ngClass]

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,7 @@ export class HomeComponent implements OnInit {
   public name = "Loc Pham"
   public age = 15;
   public traiCay = ['Tao', 'Nho', 'Cam', 'Quyt']
+  public traiCay2 = [{ ten: 'Tao', gia: -100, haGia: true }, { ten: 'Nho', gia: 900, haGia: false }, { ten: 'Cam', gia: 200, haGia: false }, { ten: 'Quyt', gia: 500, haGia: true }]
 
   constructor() { }
   public ngOnInit(): void {
