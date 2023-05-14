1160a8096efbaaf248509dd1af80cf6d81e190e4
Lộc <locpham050100@gmail.com>
2023-05-14 20:05:28 +0700

    Cascading ComboBox. Viết danh sách chọn tỉnh/thành rồi quận/huyện

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,19 +3,93 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public name = "Loc Pham"
+  public name = 'Loc Pham';
   public age = 15;
-  public traiCay = ['Tao', 'Nho', 'Cam', 'Quyt']
-  public traiCay2 = [{ ten: 'Tao', gia: -100, haGia: true }, { ten: 'Nho', gia: 900, haGia: false }, { ten: 'Cam', gia: 200, haGia: false }, {
-    ten: 'Quyt', gia: 500, haGia: true
-  }]
+  public traiCay = ['Tao', 'Nho', 'Cam', 'Quyt'];
+  public traiCay2 = [
+    { ten: 'Tao', gia: -100, haGia: true },
+    { ten: 'Nho', gia: 900, haGia: false },
+    { ten: 'Cam', gia: 200, haGia: false },
+    {
+      ten: 'Quyt',
+      gia: 500,
+      haGia: true,
+    },
+  ];
 
-  constructor() { }
+  public districts: string[] = ['quan / huyen'];
+  public vietnamData = [
+    { city: 'Chon TP', district: ['Quan / Huyen'] },
+    {
+      city: 'An Giang',
+      district: [
+        'Thành phố Long Xuyên',
+        'Thành phố Châu Đốc',
+        'Thị xã Tân Châu',
+        'Huyện An Phú',
+        'Huyện Châu Phú',
+        'Huyện Châu Thành',
+        'Huyện Chợ Mới',
+        'Huyện Phú Tân',
+        'Huyện Thoại Sơn',
+        'Huyện Tịnh Biên',
+        'Huyện Tri Tôn',
+      ],
+    },
+    {
+      city: 'Bà Rịa - Vũng Tàu',
+      district: [
+        'Thành phố Vũng Tàu',
+        'Thị xã Bà Rịa',
+        'Thị xã Phú Mỹ',
+        'Huyện Châu Đức',
+        'Huyện Côn Đảo',
+        'Huyện Đất Đỏ',
+        'Huyện Long Điền',
+        'Huyện Tân Thành',
+        'Huyện Xuyên Mộc',
+      ],
+    },
+    {
+      city: 'Bạc Liêu',
+      district: [
+        'Thành phố Bạc Liêu',
+        'Huyện Đông Hải',
+        'Huyện Giá Rai',
+        'Huyện Hòa Bình',
+        'Huyện Hồng Dân',
+        'Huyện Phước Long',
+        'Huyện Vĩnh Lợi',
+      ],
+    },
+  ];
+
+  constructor() {}
   public ngOnInit(): void {
-    console.log('trai cay = ', this.traiCay)
+    console.log('city = ', this.vietnamData);
   }
 
+  public changeCity(event: any): void {
+    const city = event.target.value;
+    console.log('event', city);
+    if (!city) {
+      return;
+    }
+
+    // cach 1
+    // const search = this.vietnamData.filter((data) => data.city === city);
+    // console.log('search', search);
+
+    // if (search && search.length > 0) {
+    //   this.districts = search[0].district;
+    // }
+
+    // cách 2
+
+    this.districts =
+      this.vietnamData.find((data) => data.city === city)?.district || [];
+  }
 }
