0f9363f2472dad48b73399f2453d548186208970
Lộc <locpham050100@gmail.com>
2023-05-17 23:27:36 +0700

    Service - Dịch vụ như tiệm tạp hóa. Cách viết + xài Services

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../Services/common.service';
 
 @Component({
   selector: 'app-about',
@@ -9,6 +10,13 @@ export class AboutComponent implements OnInit {
   public loginName = 'admin';
   public myColor = 'red';
 
-  constructor() {}
-  ngOnInit() {}
+  public counter = 0;
+  public counterBinhPhuong = 0;
+
+  constructor(private common: CommonService) {}
+  ngOnInit(): void {
+    this.counter = this.common.counter;
+    this.counterBinhPhuong = this.common.binhPhuong(this.counter);
+    this.common.counter++;
+  }
 }
